a601ab1adc9b69d8868108dd93ead64e4f0f40a7
AbigaleCallins <alcallins@gmail.com>
2022-03-17 02:58:49 -0500

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import Greeter from "./componets/Greeter";
+
+const App = () => {
+    const [username, setUsername] = useState("")
+    const [loaded, setLoaded] = useState(false);
+
+    console.log(username)
+
+     useEffect(() => {
+        setTimeout(() => {
+            setLoaded(true)
+        }, 3000);
+    }, []);
+    
+   
+    if (loaded) {
+        return (
+            <>
+                <h1 style={{ fontFamily: "sans-serif" }}>Welcome, we've been waiting for you.</h1>
+
+                <Greeter name="Abby" phrase="Hello" />
+                <Greeter name="Dalphine" phrase="Hi" />
+                <Greeter name="Scobby" phrase="scobby snacks" />
+
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)}/>
+
+                <p>Welcome, {username} </p>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <h1>Web loading...</h1>
+                <button onclick={() => setLoaded(true)}>Relaod</button>
+            </>
+        )
+    }
+}
+
+export default App;
---
+++ src/componets/Greeter.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Greeter = props => {
+    return (
+        <h2>{props.name} {props.phrase}!</h2>
+    )
+}
+export default Greeter;
