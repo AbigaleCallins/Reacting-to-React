e9a49dc340d406e614dfc97fa0482bbdff0ad366
AbigaleCallins <alcallins@yahoo.com>
2022-03-21 04:26:55 -0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Greeter from "./componets/Greeter";
 
+//  function component named "App", and return a simple JSX element
 const App = () => {
     const [username, setUsername] = useState("")
     const [loaded, setLoaded] = useState(false);
@@ -18,12 +19,12 @@ const App = () => {
         return (
             <>
                 <h1 style={{ fontFamily: "sans-serif" }}>Welcome, we've been waiting for you.</h1>
-
+                {/* from the greeter function define props  */}
                 <Greeter name="Abby" phrase="Hello" />
                 <Greeter name="Dalphine" phrase="Hi" />
                 <Greeter name="Scobby" phrase="scobby snacks" />
 
-                <input type="text" value={username} ""onChange={e => setUsername(e.target.value)}/>
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)}/>
 
                 <p>Welcome, {username} </p>
             </>
--- src/componets/Greeter.jsx
+++ src/componets/Greeter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+// receive 2 props will return a header in jsx
 const Greeter = props => {
     return (
         <h2>{props.name} {props.phrase}!</h2>
